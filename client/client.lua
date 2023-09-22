781d86a4359f8d030f85c2ae7fbddebb8478ba70
NNakreSS <87872407+NNakreSS@users.noreply.github.com>
2023-09-22 15:12:27 +0300

    .

--- client/client.lua
+++ client/client.lua
@@ -58,29 +58,3 @@ function GetMiniGame()
 end
 
 exports("GetMiniGame", GetMiniGame)
-
-
-function helalknk()
-    print("helal len ")
-end
-
-RegisterCommand("testMiniGame", function()
-    miniGame.Start({
-            -- difficultyFactor = 0.98,
-            -- lineSpeedUp = 1,
-            time = 30,
-            -- halfSuccessMin = 70,
-            valueUpSpeed = 1,
-            -- valueDownSpeed = 0.3,
-            -- areaMoveSpeed = "0.5",
-            -- img = "img/fire.webp",
-            -- areaColor = "red",
-        },
-        function()
-            helalknk()
-        end,
-        function()
-            print("beceremedinla")
-        end
-    )
-end)
